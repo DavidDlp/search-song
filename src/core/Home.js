c57c1a2b36d69ce735e88599b19dfa60d30e0204
David Dlp <dlpdavid17@gmail.com>
2022-02-27 10:20:54 +0100

    add routes

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 //components
 import SongSearch from "./../pages/SongSearch";
+import SongDetails from "../pages/SongDetails";
 
 const Home = () => {
   return (
     <div>
-      <SongSearch />
+      <div>
+        <h1>Esto es el home </h1>
+      </div>
+      <Routes>
+        <Route path="/song-search" element={<SongSearch />} />
+        <Route path="/favorite-song" element={<SongDetails />} />
+      </Routes>
     </div>
   );
 };
