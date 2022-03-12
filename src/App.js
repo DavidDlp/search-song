565d94fea894f87feb8bb3fdad678a64f5cae9a8
David Dlp <dlpdavid17@gmail.com>
2022-03-12 12:25:25 +0100

    modified position routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 //Styles
 import "./styles/app.scss";
+import { Route, Routes } from "react-router-dom";
+
 //components
-import Home from "./core/Home";
 import NavBar from "./core/NavBar";
 import Footer from "./core/Footer";
+
+//Routes
+import Home from "./pages/Home";
+import SongSearch from "./pages/SongSearch";
+import FavoriteSong from "./pages/FavoriteSong";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/song-search" element={<SongSearch />} />
+        <Route path="/favorite-song" element={<FavoriteSong />} />
+      </Routes>
       <Footer />
     </div>
   );
