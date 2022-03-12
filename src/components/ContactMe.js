70fd56433eb4b7142b18bb1620695ba3a98b8cc2
David Dlp <dlpdavid17@gmail.com>
2022-03-12 12:25:49 +0100

    modified components

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -4,30 +4,20 @@ import { FaGithub, FaLinkedin, FaMailBulk } from "react-icons/fa";
 const ContactMe = () => {
   return (
     <>
-      <div className="container">
-        <div className="container__box">
-          <a
-            href="https://github.com/DavidDlp"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaGithub className="container__box--logo" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/david-dlp"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaLinkedin className="container__box--logo" />
-          </a>
-          <a
-            href="mailto:dlpdavid17@gmail.com"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaMailBulk className="container__box--logo" />
-          </a>
-        </div>
+      <div>
+        <a href="https://github.com/DavidDlp" target="_blank" rel="noreferrer">
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/david-dlp"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+        <a href="mailto:dlpdavid17@gmail.com" target="_blank" rel="noreferrer">
+          <FaMailBulk />
+        </a>
       </div>
     </>
   );
---
+++ src/components/FavoriteTableRow.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const FavoriteTableRow = () => {
+  return (
+    <>
+      <tr>
+        <td>nombre del artista</td>
+        <td>letra de la cación</td>
+        <td>
+          <button>Ver</button>
+          <button>Eliminar</button>
+        </td>
+      </tr>
+    </>
+  );
+};
+
+export default FavoriteTableRow;
