eb8b5fe08b3f757544abe1899970bbb208eac6a9
David Dlp <dlpdavid17@gmail.com>
2022-03-13 13:18:37 +0100

    add funcrionality

--- src/pages/SongDetails.js
+++ src/pages/SongDetails.js
@@ -4,12 +4,35 @@ import React from "react";
 import SongArtist from "../components/SongArtist";
 import SongLyrics from "../components/SongLyrics";
 
-const SongDetails = () => {
+//Utils
+import Message from "./../servicies/Message";
+
+const SongDetails = ({ search, lyric, bio }) => {
+  if (!lyric || !bio) return null;
+
   return (
-    <div>
-      <SongArtist />
-      <SongLyrics />
-    </div>
+    <>
+      <div className="gnl-box__detailsBox">
+        {bio.artists ? (
+          <SongArtist artist={bio.artists[0]} />
+        ) : (
+          <Message
+            msg={`There is not interpreter ${search.artist}`}
+            bgColor="#dc3545"
+          />
+        )}
+      </div>
+      <div>
+        {lyric.error || lyric.err || lyric.name === "AbortError" ? (
+          <Message
+            msg={`The song does not exist ${search.song}`}
+            bgColor="#dc3545"
+          />
+        ) : (
+          <SongLyrics title={search.song} lyrics={lyric.lyrics} />
+        )}
+      </div>
+    </>
   );
 };
 
--- src/pages/SongSearch.js
+++ src/pages/SongSearch.js
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //Components
-import SongForm from "../components/SongForm";
+import SongForm from "./../components/SongForm";
 import SongDetails from "./SongDetails";
 
+//Utils
+import { helpHttp } from "./../servicies/helpHttp";
+import Loader from "./../servicies/Loader";
+
 const SongSearch = () => {
+  const [search, setSearch] = useState(null);
+  const [lyric, setLyric] = useState(null);
+  const [bio, setBio] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (search === null) return;
+
+    const searchData = async () => {
+      const { artist, song } = search;
+
+      let artistDb = `https://www.theaudiodb.com/api/v1/json/2/search.php?s=${artist}`;
+      let songDb = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+
+      console.log(artistDb, songDb);
+      setLoading(true);
+
+      const [artistData, songData] = await Promise.all([
+        helpHttp().get(artistDb),
+        helpHttp().get(songDb),
+      ]);
+
+      console.log(artistData, songData);
+      setBio(artistData);
+      setLyric(songData);
+      setLoading(false);
+    };
+
+    searchData();
+  }, [search]);
+
+  const handleSearch = (data) => {
+    // console.log(data);
+    setSearch(data);
+  };
+
   return (
     <div className="gnl-box">
-      <h3>Song Search</h3>
-      <SongForm />
-      <SongDetails />
+      <SongForm handleSearch={handleSearch} />
+      {loading && <Loader />}
+      {search && !loading && (
+        <SongDetails search={search} lyric={lyric} bio={bio} />
+      )}
     </div>
   );
 };
