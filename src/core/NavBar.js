fae1602a97e4ca84025cef50d6d8d635e00b5a33
David Dlp <dlpdavid17@gmail.com>
2022-04-09 12:01:21 +0200

    add icon hambur and style

--- src/core/NavBar.js
+++ src/core/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
@@ -9,7 +10,10 @@ const NavBar = () => {
           Search<span className="container-header__title--text">Song</span>
         </h1>
       </div>
-      <navbar>
+      <navbar className="container-header__navbar">
+        <button className="container-header__navbar--toggle">
+          <GiHamburgerMenu />
+        </button>
         <Link to="/">Home</Link>
         <Link to="/song-search">Search</Link>
         <Link to="/favorite-song">Favorite</Link>
