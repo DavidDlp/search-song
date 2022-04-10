d6f9c0c85b8a0a23122a31b5fe5cca0b369dedaf
David Dlp <dlpdavid17@gmail.com>
2022-04-10 12:05:46 +0200

    navbar responsive operative

--- src/core/NavBar.js
+++ src/core/NavBar.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
   return (
     <div className="container-header">
       <div className="container-header__title">
@@ -10,10 +12,16 @@ const NavBar = () => {
           Search<span className="container-header__title--text">Song</span>
         </h1>
       </div>
-      <navbar className="container-header__navbar">
-        <button className="container-header__navbar--toggle">
-          <GiHamburgerMenu />
-        </button>
+      <button
+        className="container-header__toggle"
+        onClick={() => setShowLinks(!showLinks)}
+      >
+        <GiHamburgerMenu />
+      </button>
+      <navbar
+        className="container-header__navbar "
+        id={showLinks ? "hidden" : ""}
+      >
         <Link to="/">Home</Link>
         <Link to="/song-search">Search</Link>
         <Link to="/favorite-song">Favorite</Link>
